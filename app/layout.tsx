a3d0fcf1d11d01416981ab8214b649fa171342a4
Finn Gärtner <finngaertner@protonmail.com>
2025-01-02 02:50:09 +0100

    fix: Add bottom padding to blog content

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function SiteLayout() {
           </Link>
         </div>
       </header>
-      <div className="px-4 md:px-0 pt-8">
+      <div className="px-4 md:px-0 py-8">
         <Outlet />
       </div>
     </div>
